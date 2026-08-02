fc5741439f4c6a70818a7ab10bd2afa86b051375
shreyaharidas <shreyaharidas15@gmail.com>
2026-08-02 12:00:18 +0530

    refactor for readability

---
+++ src/constants/finance.js
@@ -0,0 +1,7 @@
+const financialSourceOwners = {
+  "Amit's Wallet": "Amit",
+  "Shreya's Wallet": "Shreya",
+  // Add more sources and owners as needed
+};
+
+const defaultSplitRatio="3:2"
---
+++ src/utils/basics.js
@@ -0,0 +1,14 @@
+function currentTime() {
+  const currentDate = new Date();
+
+  const formattedDate = currentDate.toLocaleString("en-GB", {
+    day: "2-digit",
+    month: "2-digit",
+    year: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: false,
+  });
+
+  return formattedDate; // Example output: "23-12-24 15:45"
+}
